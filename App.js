bf6d2710f8764a20363d2fb30f9b21123b9fa74a
ascaryaaa <allaamascarya@gmail.com>
2024-02-28 09:42:49 +0700

    implemented if clicked the button cannot be clicked for a seccond

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  function getAccount() {
+    setLoading(true);
+    fetch("https://private-anon-75f39ba77b-itodpbni.apiary-mock.com/account")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Response was not ok!");
+        } else {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        console.log("res", res);
+        setData(res);
+      })
+      .catch((err) => {
+        console.error("err", err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button disabled={loading} onPress={getAccount} title="Click" />
+      {loading && <ActivityIndicator size="large" animating={loading} />}
+      <Text>Name: {data?.user.name}</Text>
+      <Text>Phone Number: {data?.user.phone}</Text>
+      <Text>
+        Balance: {data?.user.balance && data.user.balance}
+      </Text>
     </View>
   );
 }
@@ -13,8 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
