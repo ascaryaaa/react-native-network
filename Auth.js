a135330cfeba0f27b824b83e53330897875f7cc5
ascaryaaa <allaamascarya@gmail.com>
2024-02-28 11:47:14 +0700

    implemented Axios POST request for user authentication

--- Auth.js
+++ Auth.js
@@ -9,28 +9,47 @@ export default function Auth() {
 
 
   const getAccount = () => {
-    setLoading(true)
+    // setLoading(true)
 
-    // username: 'kminchelle',
-    // password: '0lelplR',
+    // // username: 'kminchelle',
+    // // password: '0lelplR',
 
-    fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            username: userName,
-            password: password,
-          })
+    // fetch('https://dummyjson.com/auth/login', {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //         username: userName,
+    //         password: password,
+    //       })
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   console.log("Response Get Account", data)
+    //   setUser(data)
+    //   setLoading(false)
+    // })
+    // .catch(error => {
+    //   console.error("Error", error)
+    //   setLoading(false)
+    // })
+
+    //////////////////////////////////////////////////////////
+    ////// USING AXIOS POST  /////////////////////////////////
+
+    setLoading(true);
+
+    axios.post('https://dummyjson.com/auth/login', {
+      username: userName,
+      password: password,
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Response Get Account", data)
-      setUser(data)
-      setLoading(false)
+    .then(function (response) {
+      console.log("Response Get Account", response.data);
+      setUser(response.data);
+      setLoading(false);
     })
-    .catch(error => {
-      console.error("Error", error)
-      setLoading(false)
+    .catch(function (error) {
+      console.error("Error", error);
+      setLoading(false);
     })
   }
 
